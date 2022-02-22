77fd8c1b34663c362143d3e342ed72996d13fcf9
Luciano Wencel <l_wencel@hotmail.com>
2022-02-22 14:53:28 -0300

    update formulario

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+/* 
+ * Formulário de cálculo de média 
+ */
+
 function calcularMedia( notas ) {
 
     let soma = 0;
@@ -11,21 +15,25 @@ function calcularMedia( notas ) {
 
 }
 
-let media;
+let media; // escopo global
 
 function aprovacao( notas ) {
 
-    let media = calcularMedia( notas );
+    let media = calcularMedia( notas ); // escopo da função
 
     let condicao = media >= 8 ? "aprovado" : "reprovado";
 
-    return "Média: " + media + " - Resultado: " + condicao;
+    return 'Média: ' + media + ' - Resultado: ' + condicao;
+
 }
+
+
+// Função Recursivas
 
 function contagemRegressiva(numero){
 
-    console.log(numero);
-
+    console.log(numero);  
+    
     let proximoNumero = numero - 1;
 
     if(proximoNumero > 0)
@@ -33,31 +41,147 @@ function contagemRegressiva(numero){
 
 }
 
-document.addEventListener('submit', function( evento ) {
+// contagemRegressiva(50);
 
-    evento.preventDefault();
-    evento.stopPropagation();
+/* 
+ * Formulário envio de dados 
+ */
+const formulario1 = document.getElementById('formulario-02');
 
-    let formulario = document.getElementById('formulario-01');
+if(formulario1)
+    formulario1.addEventListener('submit', function( evento ){
 
-    let dados = new FormData(formulario);
+        evento.preventDefault();
+        evento.stopPropagation();
 
-    let objeto = {};
+        if( this.getAttribute('class').match(/'erro'/) ) {
+            return false;
+        }
+        
+        let dados = new FormData(this);
 
-    let notas = [];
+        let notas = [];
 
-    for(let key of dados.keys()) {
-        objeto[key] = dados.get(key);
+        for(let key of dados.keys()) {
 
-        notas.push( parseInt(dados.get(key)));
-    }
+            let numero = dados.get(key).match(/\d*/) ? Number(dados.get(key)) : 0; // é um número
 
-    console.log(notas);
+            if(!isNaN(numero)) {
+                notas.push(numero);
+            }
 
-    console.log(objeto);
+        }
 
-    texto = aprovacao(notas);
+        console.log(notas);
 
-    document.getElementById('resultado').innerHTML = texto;
-    aprovacao(notas);
-});
+        texto = aprovacao(notas)
+
+        document.getElementById('resultado').innerHTML = texto;
+
+    });
+
+
+function validaCampo(elemento){
+
+    elemento.addEventListener('focusout', function(event) {
+
+        event.preventDefault();
+
+        if(this.value == ""){
+            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em vermelho";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        } else {
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        }
+
+    });
+
+}
+
+function validaCampoNumerico(elemento){
+
+    elemento.addEventListener('focusout', function(event) {
+
+        event.preventDefault();
+
+        let numero = this.value.match(/^[\d]5-[\d]3/) ? this.value.replace(/-/, "") : this.value; 
+
+        if(numero != "" && numero.match(/[0-9]*/) && numero >= 0 && numero <= 10){
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em destaque";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+
+    });
+
+}
+
+
+function validaEmail(elemento){
+
+    elemento.addEventListener('focusout', function(event) {
+
+        event.preventDefault();
+
+        const emailValido = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?/i;
+        if(this.value.match(emailValido)) {
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em destaque";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+
+    });
+
+}
+
+function validaUf(elemento){
+
+    elemento.addEventListener('focusout', function(event) {
+
+        event.preventDefault();
+
+        const ufValido = /^[a-z]+$/i;
+        if(this.value.match(ufValido)) {
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em destaque";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+
+    });
+
+}
+
+let camposObrigatorios = document.querySelectorAll('input.obrigatorio');
+let camposNumericos = document.querySelectorAll('input.numero');
+let camposEmail = document.querySelectorAll('input.email');
+
+for( let emFoco of camposObrigatorios) {
+    validaCampo(emFoco);
+}
+
+for( let emFoco of camposNumericos) {
+    validaCampoNumerico(emFoco);
+}
+
+for( let emFoco of camposEmail) {
+    validaEmail(emFoco);
+}
